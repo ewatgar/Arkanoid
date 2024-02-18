f2bfe4d113cb80c74ac20f1ef5b837947f9e1b08
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-02-18 13:02:03 +0100

    drag and drop with comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidBody;
+    CapsuleCollider2D capsuleCollider;
+    public float speed = 10;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
+        keyboardControls();
+    }
+
+    void OnMouseDrag()
+    {
+        mouseControls();
+    }
+
+
+    private void keyboardControls()
+    {
+        float dx = Input.GetAxis("Horizontal");
+        rigidBody.velocity = dx * speed * transform.right;
+    }
+
+    private void mouseControls()
+    {
+        Vector2 mouseCoordsPixels = Input.mousePosition;
+        Vector2 mouseCoordsGlobal = Camera.main.ScreenToWorldPoint(mouseCoordsPixels);
         
+        Vector2 platformCoords = new Vector2(mouseCoordsGlobal.x,transform.position.y);
+        
+        /*
+        Vector2 startPosition = transform.position;
+        Vector2 finalPosition = mouseCoordsGlobal;
+        Vector2 movementVector = finalPosition - startPosition;
+        Vector2 platformCoords = new Vector2(movementVector.x,transform.position.y);*/
+        rigidBody.MovePosition(platformCoords);
     }
 }
